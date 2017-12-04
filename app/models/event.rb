982628621121b09bdd7346bcfded2224acc6f945
Filip <filip.potocnik@me.com>
2017-12-04 15:43:35 +0100

    added validates_overlap gem, event overlap validation, edited datetime picker

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :start_time, presence: true
+  validates :start_time, :end_time, :overlap => {:exclude_edges => ["start_time", "end_time"]}
 end
