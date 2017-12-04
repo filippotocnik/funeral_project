b7686c4d8f65051acad709b205c56b83665e7c15
Filip Potočnik <filippotocnik@users.noreply.github.com>
2017-12-04 16:04:07 +0100

    Revert "Event validation"

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,3 @@
 class Event < ApplicationRecord
   belongs_to :user
-
-  validates :name, presence: true
-  validates :start_time, presence: true
-  validates :start_time, :end_time, :overlap => {:exclude_edges => ["start_time", "end_time"]}
 end
